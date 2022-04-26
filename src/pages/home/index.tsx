4ec0e1ef0281ca3c1cba778909ffce32f4a50891
dongyuexiang <dongyuexiang@bytedance.com>
2022-04-26 20:08:31 +0800

    333

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,44 @@
-import styles from './index.scss';
+import styles from './index.scss'
 
- function Home() {
+function Home() {
   return (
-    <div className="">
-      
+    <div className="flex flex-row" style={{ backgroundColor: '#453559' }}>
+      <div className="w-2/3 flex flex-col items-center">
+        <div className='bg-purple p-10 w-10/12 rounded-3xl'>
+          <div className="text-white font-game">Hi, 0xC109acbRa45csd63... Welcome Home!</div>
+          <div>
+            <div className='text-white'>2400 followed</div>
+            <div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>BLUE Ghost</div>
+            <div>0P8zs63dac...</div>
+            <div></div>
+            <div></div>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div className="w-1/3 bg-black">
+        <div className="">
+          <div className="font-game uppercase">Friends</div>
+        </div>
+        <div>
+          <div className="font-game uppercase">recommend neighbors</div>
+          <div>
+            <div></div>
+          </div>
+        </div>
+        <div>
+          <div>Go to the community</div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default Home
