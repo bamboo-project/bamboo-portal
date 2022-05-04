6f78aef4007cb2ffe85f6bfc3fff05b0ad439592
“zhypower1997” <854091383@qq.com>
2022-05-04 18:02:20 +0800

    feat: update style1.5

--- src/pages/index/components/neighbors/index.tsx
+++ src/pages/index/components/neighbors/index.tsx
@@ -5,7 +5,8 @@ function Neighbors() {
     items.push(
       <div
         key={index}
-        className="flex flex-row items-center pl-4 mb-4 shadow-md py-4 bg-black bg-opacity-10 rounded-lg"
+        className="w-56 flex flex-row items-center pl-4 mb-4 shadow-md py-4 bg-black bg-opacity-10 rounded-lg bg-contain bg-no-repeat"
+        style={{ backgroundImage: 'url(https://imgs.bamboownft.com/temp/index_ner_bg.png)' }}
       >
         <div className="relative">
           <div className="w-12 h-12 rounded-full bg-gray-100"></div>
@@ -21,7 +22,7 @@ function Neighbors() {
   return (
     <div className="warp-5 container mx-auto mt-14">
       <div className="font-px text-primary text-2xl uppercase">Already settled Star neighbors</div>
-      <div className="grid grid-cols-6 grid-flow-row gap-4 mt-5">{items}</div>
+      <div className="grid grid-cols-5 grid-flow-row gap-4 mt-5">{items}</div>
     </div>
   )
 }
